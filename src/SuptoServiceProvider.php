170eeb3e98a7801627486b9e791c3434188c0841
Supto Purkayasto <developersuptop@gmail.com>
2020-05-28 02:12:39 +0600

    laravel preset for @metarial-ui

---
+++ src/SuptoServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Laracasts\LaravelPreset;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Console\PresetCommand;
+
+class SuptoServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        PresetCommand::macro('laracasts', function ($command) {
+            Preset::install();
+
+            $command->info('All finished! Please compile your assets, and you are all set to go!');
+        });
+    }
+}
